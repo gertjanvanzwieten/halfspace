6539f3ac7ab62054d61e113cb187a775e176c8f7
Gertjan van Zwieten <gjvanzwieten@hvzengineering.nl>
2016-12-19 14:10:56 +0100

    make installable via setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 #! /usr/bin/env python3
 
-import setuptools
+import setuptools, setuptools.command.build_py
+import os, platform
+
+compilecmd = {
+  'Linux': 'gcc -shared -o {dst:}.so -lc -fpic -g -O3 -march=native -mtune=native {src:}.c',
+  'Windows': 'cl /LD /DDLL {src:}.c /OUT:{dst:}.dll',
+}[ platform.system() ]
+
+class build_py( setuptools.command.build_py.build_py ):
+  def run(self):
+    setuptools.command.build_py.build_py.run( self )
+    print( 'compiling libokada' )
+    assert 0 == os.system( compilecmd.format(
+      src=os.path.join('halfspace','libokada'),
+      dst=os.path.join(self.build_lib,'halfspace','libokada') ) )
 
 setuptools.setup(
   name='halfspace',
   version='1.9',
   author='Gertjan van Zwieten',
-  packages=['halfspace'],
+  packages=[ 'halfspace' ],
+  cmdclass={ 'build_py': build_py },
 )
